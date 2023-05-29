a2fb4817e91a8bba7f4028482df0fb0ee1832113
diogosantos6 <126776577+diogosantos6@users.noreply.github.com>
2023-05-29 15:51:35 +0100

    Modify files for miguel branch

--- resources/views/tshirtimages/index.blade.php
+++ resources/views/tshirtimages/index.blade.php
@@ -1,44 +1,35 @@
 @extends('template.layout')
 
-@section('main')
-    {{-- <form method="GET" action="{{ route('tshirtimages.index') }}">
-        <div class="d-flex justify-content-between">
-            <div class="flex-grow-1 pe-2">
-                <div class="d-flex justify-content-between">
-                    <div class="flex-grow-1 mb-3 form-floating">
-                        <select class="form-select" name="category" id="inputCategory">
-                            <option {{ old('category', $filterByCategory) === '' ? 'selected' : '' }} value="">
-                                Todas as Categorias </option>
+
+@section('main_filters')
+    <form method="GET" action="{{ route('tshirtimages.index') }}">
+
+            <div class="container" id="filter_container">
+                <label for="inputCategory" class="form-label">Categoria</label>
+                    <select class="form-select" name="category" id="FilterinputCategory">
+                        <option {{ old('category', $filterByCategory) === '' ? 'selected' : '' }} value=""> Todas as Categorias </option>
                             @foreach ($categories as $category)
                                 <option {{ old('departamento', $filterByCategory) == $category->name ? 'selected' : '' }}
                                     value="{{ $category->name }}">{{ $category->name }}</option>
                             @endforeach
-                        </select>
-                        <label for="inputCategory" class="form-label">Categoria</label>
-                    </div>
-                </div>
-                <div class="d-flex justify-content-between">
-                    <div class="mb-3 me-2 flex-grow-1 form-floating">
-                        <input type="text" class="form-control" name="name" id="inputName"
-                            value="{{ old('name', $filterByName) }}">
-                        <label for="inputName" class="form-label">Nome</label>
-                    </div>
-                </div>
-                <div class="d-flex justify-content-between">
-                    <div class="mb-3 me-2 flex-grow-1 form-floating">
-                        <input type="text" class="form-control" name="description" id="inputDescription"
-                            value="{{ old('description', $filterByDescription) }}">
-                        <label for="inputDescription" class="form-label">Descrição</label>
-                    </div>
-                </div>
+                    </select>
+                    <br>
+
+                    <label for="inputName" class="form-label">Nome</label>
+                    <input type="text" class="form-control" name="name" id="FilterinputName" value="{{ old('name', $filterByName) }}">
+                    <br>
+
+                    <label for="inputDescription" class="form-label">Descrição</label>
+                    <input type="text" class="form-control" name="description" id="FilterinputDescription" value="{{ old('description', $filterByDescription) }}">
+                    <br>
+
+                    <button type="submit" class="btn btn-info mb-3 px-4 flex-grow-1" name="filtrar">Filtrar</button>
+                    <a href="{{ route('tshirtimages.index') }}" class="btn btn-secondary mb-3 px-4 flex-grow-1">Limpar</a>
             </div>
-            <div class="flex-shrink-1 d-flex flex-column justify-content-between">
-                <button type="submit" class="btn btn-primary mb-3 px-4 flex-grow-1" name="filtrar">Filtrar</button>
-                <a href="{{ route('tshirtimages.index') }}"
-                    class="btn btn-secondary mb-3 py-3 px-4 flex-shrink-1">Limpar</a>
-            </div>
-        </div>
-    </form> --}}
+
+    </form>
+@endsection
+@section('main')
     @foreach ($tshirtimages as $tshirtimage)
         <div class="col mb-5">
             <div class="card h-100">
