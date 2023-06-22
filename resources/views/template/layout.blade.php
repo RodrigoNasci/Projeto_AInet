5e05f00c2d2e880399dd5860bcb9159e8b4acc60
Rodrigo Nascimento <2200704@my.ipleiria.pt>
2023-06-22 22:54:17 +0100

    clean up

--- resources/views/template/layout.blade.php
+++ resources/views/template/layout.blade.php
@@ -32,19 +32,6 @@
                             <a class="nav-link active" aria-current="page" href="{{ route('orders.index') }}">Employee Panel</a>
                         </li>
                     @endif
-                    <li class="nav-item"><a class="nav-link" href="#!">About</a></li> <!-- about page-->
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">Shop</a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="#!">All Products</a></li>
-                            <li>
-                                <hr class="dropdown-divider" />
-                            </li>
-                            <li><a class="dropdown-item" href="#!">Popular Items</a></li>
-                            <li><a class="dropdown-item" href="#!">New Arrivals</a></li>
-                        </ul>
-                    </li>
                 </ul>
                 {{-- <form class="d-flex" action="{{ route('cart.show') }}">
                     <button class="btn btn-outline-dark" type="submit">
